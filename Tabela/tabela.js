1688a41587ff7977f9fa1e95111ed847d7eaa074
Andre Mai Velasco <andremaivelasco@hotmail.com>
2022-11-24 00:27:42 -0300

    adicionando linha na tabela

--- Tabela/tabela.js
+++ Tabela/tabela.js
@@ -1,9 +1,21 @@
 let adicionar = document.getElementById("adicionar");
+let excluir = document.getElementById("excluir");
 let tabela = document.getElementById("tabela");
 
 adicionar.addEventListener("click", add);
 
 function add(){
-    tabela.appendChild(tr);
-    prompt(Escreve);
+    let linha = document.createElement("tr");
+    let elementos = [];
+
+    for(var i=0; i<4; i++)
+    {
+        let campos = ["id", "nome", "curso", "e-mail"];
+        elementos[i] = document.createElement("td");
+        elementos[i].innerText = prompt(`Digite o ${campos[i]} do aluno`);
+
+        linha.appendChild(elementos[i]);
+    }
+    
+    tabela.appendChild(linha);
 }
